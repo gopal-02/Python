1d89aad6e322cd70c8edd2e599141167337cd8a3
gopal8220 <gopalkumar8220@gmail.com>
2022-10-10 23:07:40 +0530

    added string functions

---
+++ string_functions.py
@@ -0,0 +1,10 @@
+story = "i am Gopal learning python and pushing my codes on github"
+
+# String Functions
+print(len(story))
+print(story.endswith("github"))
+print(story.count("e"))
+print(story.capitalize())
+print(story.find("on"))
+print(story.replace("Gopal", "Gopal kumar"))
+ 
