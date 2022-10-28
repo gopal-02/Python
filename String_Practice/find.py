0e573476814b950f5cb2f6847cd8f9290a61fdbc
gopal8220 <gopalkumar8220@gmail.com>
2022-10-29 00:15:39 +0530

    initial commit

---
+++ String_Practice/find.py
@@ -0,0 +1,16 @@
+S="Python is a programming language"
+a=input("a: ")
+b=input("b: ")
+c=input("c: ")
+if a in S:
+    print(True)
+else:
+    print(False) 
+if b in S:
+    print(True)
+else:
+    print(False)  
+if c in S:
+    print(True)
+else:
+    print(False)             
